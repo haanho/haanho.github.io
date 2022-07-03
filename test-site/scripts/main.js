482b0b2d088c4581fa92c2a356db11e0dbcb7909
haanho <94474961+haanho@users.noreply.github.com>
2022-07-04 01:33:08 +0700

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,28 @@
+let myImage = document.querySelector('img');
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === "images/kotik-cannabis.png" ) {
+        myImage.setAttribute('src', 'images/61DkhkBCVtL.jpg');
+    } else {
+        myImage.setAttribute('src', "images/kotik-cannabis.png");
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+        setUserName();
+    } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = "Wassup'," + myName;
+    }
+}
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = "Wassup'," + storedName;
+}myButton.onclick = function() {
+    setUserName();
+}
